1e597d10c40c0dc37298083c422efc9ec6958a80
Dmitry Malikov <malikov.d.y@gmail.com>
2013-12-11 02:07:46 +0400

    Sepate some logic into a Sandbox module

--- src/Main.hs
+++ src/Main.hs
@@ -1,59 +1,36 @@
-{-# LANGUAGE NoOverloadedStrings, ScopedTypeVariables #-}
-import           Control.Applicative
+{-# LANGUAGE NoOverloadedStrings #-}
 import           Options.Applicative
-import           System.Directory
-import           System.FilePath.Posix
-import           System.IO.Temp
-import           System.Process
+import           System.Directory    (getCurrentDirectory, getTemporaryDirectory, setCurrentDirectory)
+import           System.IO.Temp      (withTempDirectory)
+import           System.Process      (callProcess)
 
 import           Git
+import           Sandbox
 
-
-try_get_sandbox_dir :: IO (Maybe FilePath)
-try_get_sandbox_dir = do
-  dir <- getCurrentDirectory
-  let sandbox_dir = dir </> ".cabal-sandbox"
-  exist <- doesDirectoryExist sandbox_dir
-  return $ if exist
-    then Just sandbox_dir
-    else Nothing
-
-cd :: String -> IO ()
-cd dir = do
-  current <- getCurrentDirectory
-  setCurrentDirectory (current </> dir)
-
-link_to_sandbox :: FilePath -> IO ()
-link_to_sandbox sandbox_dir = callProcess "cabal" ["sandbox", "init", "--sandbox", sandbox_dir]
-
-with_temp_dir :: (FilePath -> IO ()) -> IO ()
-with_temp_dir f = do
-  temp_dir <- getTemporaryDirectory
-  withTempDirectory temp_dir "cabalg_X" f
-
-
-install :: Configuration -> IO ()
-install (Configuration { _git_url = url, _branch = branch }) = do
-  back <- getCurrentDirectory
-  with_temp_dir $ \dir -> do
-    clone url branch dir
-    sandbox_dir <- try_get_sandbox_dir
-    cd dir
-    case sandbox_dir of
-      Just dir' -> link_to_sandbox dir'
-      Nothing   -> return ()
-    callProcess "cabal" ["install"]
-  setCurrentDirectory back
-
-data Configuration = Configuration { _git_url :: String, _branch :: Maybe String }
-
-opt_parser :: Parser Configuration
-opt_parser = Configuration
-  <$> argument str (metavar "REPO")
-  <*> optional (strOption (long "branch" <> short 'b'))
 
 main :: IO ()
 main = execParser opts >>= install
   where opts = info (helper <*> opt_parser) $ fullDesc <>
           progDesc "Install cabal package from git to cabal globally or in sandbox if it exists"
 
+
+data Configuration = Configuration { _git_url :: String, _branch :: Maybe String }
+
+install :: Configuration -> IO ()
+install (Configuration { _git_url = url, _branch = branch }) = do
+  back <- getCurrentDirectory
+  temp_dir <- getTemporaryDirectory
+  withTempDirectory temp_dir "cabalg_X" $ \dir -> do
+    clone url branch dir
+    sandbox_dir <- try_get_sandbox_dir back
+    setCurrentDirectory dir
+    case sandbox_dir of
+      Just dir' -> link_to_sandbox dir'
+      Nothing   -> return ()
+    callProcess "cabal" ["install"]
+  setCurrentDirectory back
+
+opt_parser :: Parser Configuration
+opt_parser = Configuration
+  <$> argument str (metavar "URL")
+  <*> optional (strOption (long "branch" <> short 'b'))
